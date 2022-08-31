59ca65ac81bc678cbc0bad5e08231302c5f190e8
tekim <rldod526@naver.com>
2022-08-31 13:51:07 +0900

    Create Project

---
+++ Note_Eval/Note_Eval/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Note_Eval
+//
+//  Created by Kim TaeSoo on 2022/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
